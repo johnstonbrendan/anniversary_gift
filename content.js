89874c9b641cb5654443e13a69f8cb0408109fef
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2022-10-17 00:34:22 -0700

    Mostly working but can't handle maing a new game and the mask text doesn't get replaced everytime

--- content.js
+++ content.js
@@ -3,6 +3,10 @@
 // console.log('new')
 card_in_hand_class = "css-ach1w2"
 
+// follow this video to setup chrome extensions:
+// https://www.youtube.com/watch?v=9Tl3OmwrSaM&list=PLRqwX-V7Uu6bL9VOMT65ahNEri9uqLWfS&index=3
+
+
 
 // printAllCards()
 
@@ -40,41 +44,74 @@ var cardNameReplacements = {
 
 // Share hand ids css-12z5dl1
 
+// Regex for replacing the "M" with "H"
+const M_to_H_regex =  /(\d+)M/g;
+
+// Word to replace mask with
+replaceMaskText = "Hug" // have to manually replace the first letter in the corner of cards in code below
+
 
 // https://stackoverflow.com/questions/2157963/is-it-possible-to-listen-to-a-style-change-event
 // Make an observer to see if the cards in the hand have change
 var handObserver = new MutationObserver(function(mutations) {
-  console.log("Here maybe")
+  // console.log("Here maybe")
   mutations.forEach(function(mutationRecord) {
       console.log('hand changed!');
+
+
+      handObserver.disconnect()
+
+
       editHand()
+      editHandMoneyCorner()
+      editHandMaskWord()
+
+      // editAllMoneyCorner()
+      startHandObserver()
   });    
 });
 
 var popUpObserver = new MutationObserver(function(mutations){
   mutations.forEach(function(mutationRecord){
     console.log("New Pop Up")
+
+    popUpObserver.disconnect()
+    
     editHand()
     editTable()
+    editPopUpMoneyCorner()
+    editMasksPopupRequestText()
+
+    startPopUpObserver()
+    
+    
+    // editAllMoneyCorner()
   })
 })
 
 var tableObserver = new MutationObserver(function(mutations) {
-  console.log("Here maybe")
   mutations.forEach(function(mutationRecord) {
       console.log('table changed!');
+      tableObserver.disconnect()
       editTable()
+      editTableMoneyCorner()
+      editMaskCountWord()
+      startTableObserver()
+
+      startTableObserver()
+      // editAllMoneyCorner()
   });    
 });
 
 // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
-waitForElementToDisplay("#your-hand",main,1000,1e7);
+waitForElementToDisplay("#your-hand",".css-1omjsz4",main,1000,1e7);
 // Wait for game to load^^^ then run main
 
-function waitForElementToDisplay(selector, callback, checkFrequencyInMs, timeoutInMs) {
+function waitForElementToDisplay(selector1,selector2, callback, checkFrequencyInMs, timeoutInMs) {
     var startTimeInMs = Date.now();
     (function loopSearch() {
-      if (document.querySelector(selector) != null) {
+
+      if (document.querySelector(selector1) != null && document.querySelector(selector1).querySelector(selector2) != null) {
         callback();
         return;
       }
@@ -89,32 +126,56 @@ function waitForElementToDisplay(selector, callback, checkFrequencyInMs, timeout
   }
 
 
+
+function waitForElementToDie(selector, callback, checkFrequencyInMs, timeoutInMs) {
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    if (document.querySelector(selector) == null) {
+      console.log('death')
+      setTimeout(() => {  callback(); }, 1000);
+      
+      return;
+    }
+    else {
+      setTimeout(function () {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs)
+          return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+}
+
+
+
 // Main function setups the for when to do the edits
 function main(hand_selector) {
     console.log("Loaded screen")
-    hand = document.getElementById("your-hand")
-    console.log(hand)
+    // hand = document.getElementById("your-hand")
+    // console.log(hand)
+
+    // printAllMoneyCorner()
     
     editHand()
     editTable()
+// editAllMoneyCorner()
+    editHandMoneyCorner()
+    editTableMoneyCorner()
+    editHandMaskWord()
+    editHandMDescrt()
+    editMaskCountWord()
+
     // hand.style.bottom = "200px"
     
-    var userHand = document.getElementById('your-hand');
-    handObserver.observe(userHand, { subtree : true , childList: true , characterData: true})//, subtree : true });
-    
-    var playersTables =  document.querySelectorAll('.player-cards.css-opvb8');
-    playersTables.forEach(table => {
-      tableObserver.observe(table, { subtree : true , childList: true , characterData: true});
-      // console.log(table) // just to see how many there are
-      // console.log("got a player table")
-    })
-    
-    var popUpRoot = document.querySelector('.css-0')
-    popUpObserver.observe(popUpRoot, { subtree : TreeWalker, childList : true} )
-    
-   
-}
 
+    
+    startHandObserver()
+    startTableObserver()
+    startPopUpObserver()
+    
+    waitForElementToDie("#other-players",waitForElementToDisplay("#your-hand",".css-1omjsz4",main,1000,1e7),3000,1e7)
+}
+// Debug function
 function printAllCards(){
   handCards = document.querySelectorAll('.css-12z5dl1')
   handCards.forEach(card => {
@@ -122,6 +183,136 @@ function printAllCards(){
   })
 }
 
+// Debug function
+function printAllMoneyCorner(){
+  moneyCorners = document.querySelectorAll('.css-1omjsz4')
+  moneyCorners.forEach(corner => {
+    console.log(corner.innerHTML)
+  })
+}
+
+function editHandMoneyCorner(){
+  handMoneyCorners = document.querySelector('#your-hand').querySelectorAll('.css-1omjsz4')
+  handMoneyCorners.forEach(corner => {
+    console.log("Found corner")
+    console.log(corner)
+    curHandCornerTet = corner.innerHTML
+    corner.innerHTML = curHandCornerTet.replace(M_to_H_regex, '$1H')
+  })
+}
+
+function editPopUpMoneyCorner(){
+
+  var window = document.querySelector('.css-vlqn3g')
+  if (window != null){
+    popUpMoneyCorners = window.querySelectorAll('.css-1omjsz4')
+    
+    popUpMoneyCorners.forEach(corner => {
+      curPopUpCornerTet = corner.innerHTML
+      corner.innerHTML = curPopUpCornerTet.replace(M_to_H_regex, '$1H')
+    })
+  }
+}
+
+
+function editTableMoneyCorner(){
+  var playersTables =  document.querySelectorAll('.player-cards.css-opvb8');
+  playersTables.forEach(table => {
+    // console.log("267")
+    tableMoneyCorners = table.querySelectorAll('.css-1omjsz4')
+    tableMoneyCorners.forEach(corner => {
+      curHandCornerTet = corner.innerHTML
+      // console.log(curHandCornerTet)
+      corner.innerHTML = curHandCornerTet.replace(M_to_H_regex, '$1H')
+  })
+  })
+}
+
+function editTableMaskWord(){
+  // TODO: make this do the table as above but with mask word
+}
+
+function editPopupMaskWord(){
+  // TODO: make this do the popup as above but with mask word
+}
+
+function editHandMaskWord(){
+  var maskCards = document.querySelector('#your-hand').querySelectorAll('.css-1f7tksj')
+  // console.log(maskCards)
+  maskCards.forEach(card => {
+    curMaskText = card.innerHTML
+    // console.log(curMaskText)
+    card.innerHTML = curMaskText.replace("Mask",replaceMaskText)
+  })
+}
+
+function editHandMDescrt(){
+  var maskCards = document.querySelector('#your-hand').querySelectorAll('.description.css-16s3zni')
+  // console.log(maskCards)
+  maskCards.forEach(card => {
+    curMaskText = card.innerHTML
+    // console.log(curMaskText)
+    card.innerHTML = curMaskText.replace(M_to_H_regex, '$1H')
+  })
+}
+
+function editPopUpMDescrt(){
+// same as above for the description like charge 5M
+}
+
+function editMasksPopupRequestText(){ // Note this doesn't work for the rainbow rent cards. Would have to dig through a lot of layers
+  var rentRectangles = document.querySelectorAll('.css-3t37o0')
+  rentRectangles.forEach(rect => {
+    var divs = rect.childNodes
+    divs.forEach(div => {
+      curText = div.innerHTML
+      div.innerHTML = curText.replace("Mask",replaceMaskText)
+    })
+  })
+}
+
+function editPopupMasksText(){
+  // When it gives a summary of the players masks and cards when choosing who to target
+}
+
+function editMaskCountWord(){
+  var maskCounters = document.querySelectorAll('.css-rk7wlr')
+  maskCounters.forEach(counter => {
+    curCounterTxt = counter.innerHTML
+    counter.innerHTML = curCounterTxt.replace("Mask",replaceMaskText)
+  })
+
+}
+
+// Setup hand observers
+function startHandObserver(){
+  var userHand = document.getElementById('your-hand');
+  handObserver.observe(userHand, { subtree : true , childList: true })//, characterData: true})//, subtree : true });
+}
+
+function startTableObserver(){
+  var playersTables =  document.querySelectorAll('.player-cards.css-opvb8');
+  playersTables.forEach(table => {
+    tableObserver.observe(table, { subtree : true , childList: true })//, characterData: true});
+    // console.log(table) // just to see how many there are
+    // console.log("got a player table")
+  })
+}
+
+function startPopUpObserver(){
+  var popUpRoot = document.querySelector('.css-0')
+  popUpObserver.observe(popUpRoot, { subtree : TreeWalker, childList : true} )
+}
+// function editAllMoneyCorner(){
+//   moneyCorners = document.querySelectorAll('.css-1omjsz4')
+//   moneyCorners.forEach(corner => {
+//     // console.log("in edit ll money corner")
+//     // console.log(corner)
+//     curCornerText = corner.innerHTML
+//     corner.innerHTML = curHandCornerTet.replace(M_to_H_regex, '$1H')
+//     // console.log(replaceText)
+//   })
+// }
 // When the player plays a card or a new card is drawn callback
 
 // When the opposing player plays a card callback
@@ -130,9 +321,10 @@ function printAllCards(){
 
 // Change the text of all the cards on the Table
 function editTable(){
-  handCards = document.querySelectorAll('.css-10em6k6')
-  handCards.forEach(card => {
-   console.log(card.innerHTML)
+  console.log("editing table")
+  tableCards = document.querySelectorAll('.css-10em6k6')
+  tableCards.forEach(card => {
+  //  console.log(card.innerHTML)
    curCardText = card.innerHTML
    if (curCardText in cardNameReplacements){
     card.innerHTML = cardNameReplacements[curCardText]
@@ -142,9 +334,10 @@ function editTable(){
 
 // Change the text of all the cards in your hand
 function editHand(){
+  console.log("editing hand")
   handCards = document.querySelectorAll('.css-12z5dl1')
   handCards.forEach(card => {
-   console.log(card.innerHTML)
+  //  console.log(card.innerHTML)
    curCardText = card.innerHTML
    if (curCardText in cardNameReplacements){
     card.innerHTML = cardNameReplacements[curCardText]
@@ -157,6 +350,11 @@ function editHand(){
 // Change all the words mask to be Hugs
 
 // Change all the Ms to Hs
+
+// Change the Masks total on the side to hugs
+
+// change the M in the action card deccriptions to H
+
 
 
 
